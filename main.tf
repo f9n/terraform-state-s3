ac53fe17ccf79f9df991c2c9f083af0b619df21f
Fatih Sarhan <f9n@protonmail.com>
2019-08-26 23:19:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,25 @@ resource "aws_s3_bucket" "terraform" {
     Environment = var.env
   }
 
+  # Enable versioning so we can see the full revision history of our state files
   versioning {
     enabled = true
+  }
+  
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
 
 resource "aws_dynamodb_table" "terraform" {
-  name           = var.dynamodb_table
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
+  name         = var.dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
